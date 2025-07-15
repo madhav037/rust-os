f980e623136c6ca992e0e93e5295bf3f2c688dcf
madhav <109799001+madhav037@users.noreply.github.com>
2025-07-15 22:57:59 +0530

    vga text mode

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,25 @@
 #![no_std]   // Disable the standard library
 #![no_main] // Disable the main function provided by the standard library
 
+mod vga_buffer;
+
 use core::panic::PanicInfo; // Import the PanicInfo type
 
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]  // don't mangle the name of this function
 pub extern "C" fn _start() -> ! { 
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    loop {}
+    println!("Hello World{}", "!");
+    panic!("This is a panic message!"); // Trigger a panic
+    // loop {}
 }
 
 
 #[panic_handler] // Define a custom panic handler
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {} // Infinite loop to halt the program on panic
 }
 
-// https://os.phil-opp.com/minimal-rust-kernel/
+// https://os.phil-opp.com/vga-text-mode/
